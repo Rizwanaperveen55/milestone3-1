3d330774ddd676f525298b06bb5a1abbf622537a
muhammad ahmed noorani <ahmednoorani258@gmail.com>
2024-11-29 21:12:08 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link';
-import { blogs } from '@/utils/blog';
-import BlogCard from '@/components/BlogCard'; // Import BlogCard
+
 import Page from '@/components/Page';
 
 export default function BlogPage() {
